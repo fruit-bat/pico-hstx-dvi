a90f75709b5912951b8b2522fd0652f8364ff005
fruit-bat <pmscull@gmail.com>
2025-08-04 21:17:45 +0100

    config

--- src/hstx_dvi_row_fifo.h
+++ src/hstx_dvi_row_fifo.h
@@ -11,8 +11,8 @@ extern "C" {
 void hstx_dvi_row_fifo_init(PIO pio, uint sm, hstx_dvi_row_t* underflow_row);
 void hstx_dvi_row_fifo_init1(PIO pio, hstx_dvi_row_t* underflow_row);
 void hstx_dvi_row_fifo_put_blocking(hstx_dvi_row_t* row);
-hstx_dvi_row_t* hstx_dvi_row_fifo_get(uint32_t row_index);
 
 #ifdef __cplusplus
 } 
 #endif
+
