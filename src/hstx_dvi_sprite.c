844261318cb5743ce2930336c196121103b7c7b0
fruit-bat <pmscull@gmail.com>
2025-08-08 21:13:17 +0100

    sprite

--- src/hstx_dvi_sprite.c
+++ src/hstx_dvi_sprite.c
@@ -22,12 +22,7 @@ void __not_in_flash_func(render_row_mono)(
 	hstx_dvi_row_t* r,
 	hstx_dvi_pixel_t p
 ) {
-	for(int32_t i = 0; i < (MODE_H_ACTIVE_PIXELS>>2); i++) {
-        hstx_dvi_row_set_pixel_quad(
-            r, 
-            i, 
-            p,p,p,p);
-	}
+    hstx_dvi_fill_row(r, p);
 }
 
 static inline void render_sprite_pixel(
@@ -58,4 +53,80 @@ static inline void render_pixel(
     hstx_dvi_row_set_pixel(r, j, p);
 }
 
+static inline void render_sprite_row_n_p1(
+	uint32_t d,
+	const Pallet1_t p1,
+	hstx_dvi_row_t* r,
+	const int32_t x,
+	const SpriteId spriteId,
+	const uint32_t w
+) {
+	if (d)
+	{
+		const uint32_t bm = 1 << (w-1);
+        const hstx_dvi_pixel_t p = p1[0];
+		if (((uint32_t)x) < (MODE_H_ACTIVE_PIXELS - w))
+		{
+			for (int32_t i = 0; i < w; i++)
+			{
+				const uint32_t j = (uint32_t)x + i;
+				if (d & bm)
+				{
+					render_sprite_pixel(r, p, spriteId, j);
+				}
+				d <<= 1;
+			}
+		}
+		else
+		{
+			for (int32_t i = 0; i < w; i++)
+			{
+				const uint32_t j = (uint32_t)x + i;
+				if ((j < MODE_H_ACTIVE_PIXELS) && (d & bm))
+				{
+					render_sprite_pixel(r, p, spriteId, j);
+				}
+				d <<= 1;
+			}
+		}
+	}
+}
+
+static inline void render_row_n_p1(
+	uint32_t d,
+	const Pallet1_t p1,
+	hstx_dvi_row_t* r,
+	const int32_t x,
+	const uint32_t w
+) {
+	if (d)
+	{
+        const hstx_dvi_pixel_t p = p1[0];
+		const uint32_t bm = 1 << (w-1);
+		if (((uint32_t)x) < (MODE_H_ACTIVE_PIXELS - w))
+		{
+			for (int32_t i = 0; i < w; i++)
+			{
+				const uint32_t j = (uint32_t)x + i;
+				if (d & bm)
+				{
+					render_pixel(r, p, j);
+				}
+				d <<= 1;
+			}
+		}
+		else
+		{
+			for (int32_t i = 0; i < w; i++)
+			{
+				const uint32_t j = (uint32_t)x + i;
+				if ((j < MODE_H_ACTIVE_PIXELS) && (d & bm))
+				{
+					render_pixel(r, p, j);
+				}
+				d <<= 1;
+			}
+		}
+	}
+}
 
