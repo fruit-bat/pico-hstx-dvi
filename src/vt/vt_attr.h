ef9d3093dc68345ab14fd6f5447ce51ed0265f34
fruit-bat <pmscull@gmail.com>
2025-08-29 15:35:40 +0100

    vt

---
+++ src/vt/vt_attr.h
@@ -0,0 +1,60 @@
+#pragma once
+/* Copyright (c) 2025 fruit-bat
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the copyright holder nor the
+ *     names of contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS,
+ * COPYRIGHT HOLDERS, OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <stdint.h>
+#include "vt_types.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define VT_ATTRS_NORMAL    (0x00)
+#define VT_ATTRS_BOLD      (0x01)
+#define VT_ATTRS_DIM       (0x02)
+#define VT_ATTRS_UNDERLINE (0x04)
+#define VT_ATTRS_BLINK     (0x08)
+#define VT_ATTRS_REVERSE   (0x10)
+#define VT_ATTRS_INVISIBLE (0x20)
+
+typedef struct
+{
+    uint8_t ch;
+    uint8_t at;
+    uint8_t bg;
+    uint8_t fg;
+} vt_attr_c_t;
+
+typedef union
+{
+    vt_attr_c_t s; // Separate
+    uint32_t c;    // Combined
+} vt_attr_t;
+
+#ifdef __cplusplus
+}
+#endif
